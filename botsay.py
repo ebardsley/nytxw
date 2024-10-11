02050abd4e58abf0d893830f3046b9e43033d142
Ed Bardsley <ebardsley@gmail.com>
2024-10-11 03:46:28 +0000

    botsay rpc support. pre-commit cleanup.

--- botsay.py
+++ botsay.py
@@ -1,57 +1,83 @@
 #!/usr/bin/env pipenv-shebang
-
+import asyncio
 import os
 import sys
+import urllib.parse
 
+import aiohttp
 import discord
 
 
-def say(token, channels, message):
-  client = discord.Client()
+async def rpc(endpoint, json):
+    async with aiohttp.ClientSession() as session:
+        try:
+            response = await session.post(endpoint, json=json)
+        except aiohttp.ClientError as e:
+            raise
 
-  @client.event
-  async def on_ready():
-    print('Logged into Discord as {0.user}'.format(client))
-    for channel_id in channels:
-      channel = client.get_channel(channel_id)
-      print('Channel', channel.id if channel else None, channel)
-      if channel:
-        await channel.send(message)
-    await client.close()
+        if not response.ok:
+            print("failed:", await response.text())
 
-  @client.event
-  async def on_error(event):
-    await client.close()
-    raise
 
-  client.run(token)
+def discord_message(token, channels, message):
+    client = discord.Client()
+
+    @client.event
+    async def on_ready():
+        print(f"Logged into Discord as {client.user}")
+        for channel_id in channels:
+            channel = client.get_channel(channel_id)
+            print("Channel", channel.id if channel else None, channel)
+            if channel:
+                await channel.send(message)
+        await client.close()
+
+    @client.event
+    async def on_error(event):
+        await client.close()
+        raise
+
+    client.run(token)
 
 
 def webhook(url, message):
-  hook = discord.SyncWebhook.from_url(url)
-  hook.send(
-    content=message,
-    # suppress_embeds=True,
-  )
+    hook = discord.SyncWebhook.from_url(url)
+    hook.send(
+        content=message,
+        # suppress_embeds=True,
+    )
 
 
 def main(argv):
-  if len(argv) < 3:
-    print(f'usage: {argv[0]} <channel id|webhook> <message>', file=sys.stderr)
-    sys.exit(1)
+    if len(argv) < 3:
+        print(f"usage: {argv[0]} <channel id|webhook> <message>", file=sys.stderr)
+        sys.exit(1)
 
-  message = ' '.join(argv[2:])
-  if argv[1].startswith('https:'):
-    print('Webhook:', message)
-    webhook(argv[1], message)
-  else:
-    import env
-    token = env.require_env('NYTXW_BOT')
-    channels = [int(argv[1])]
-    print('Message:', message)
-    say(token, channels, message)
+    message = " ".join(argv[2:])
+    return say(argv[1], message)
 
 
+def say(target, message):
+    if target.startswith("https:"):
+        print("Webhook:", message)
+        webhook(target, message)
 
-if __name__ == '__main__':
-  main(sys.argv)
+    elif target.startswith("http:"):  # TODO: https-ify?
+        parsed = urllib.parse.urlparse(target)
+        json = dict(urllib.parse.parse_qsl(parsed.query), msg=message)
+        parsed = parsed._replace(query=None)
+        endpoint = urllib.parse.urlunparse(parsed)
+        print("RPC", endpoint, json)
+        asyncio.run(rpc(endpoint, json))
+
+    else:
+        import env
+
+        token = env.require_env("NYTXW_BOT")
+        channels = [int(c) for c in target.split(",") if c]
+        print("Message:", message)
+        discord_message(token, channels, message)
+
+
+if __name__ == "__main__":
+    main(sys.argv)
--- leaderboards.py
+++ leaderboards.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env pipenv-shebang
-
 import contextlib
 import json
 import os
@@ -13,90 +12,102 @@ import botsay
 import env
 
 
-LEADERBOARD_URL = 'https://www.nytimes.com/svc/crosswords/v6/leaderboard/mini.json'
-COOKIES = {'NYT-S': env.require_env('NYTXW_COOKIE')}
-BOT_TOKEN = env.require_env('NYTXW_BOT')
-CHANNEL_ID = [int(c) for c in os.getenv('NYTXW_CHANNELS', '').split(',') if c]
-DB = os.getenv('NYTXW_SQLITE3', 'data/mini.sqlite3')
+LEADERBOARD_URL = "https://www.nytimes.com/svc/crosswords/v6/leaderboard/mini.json"
+COOKIES = {"NYT-S": env.require_env("NYTXW_COOKIE")}
+CHANNEL_ID = os.getenv("NYTXW_CHANNELS", "")
+DB = os.getenv("NYTXW_SQLITE3", "data/mini.sqlite3")
+
 
 def time_in_seconds(t):
-  if isinstance(t, str):
-    if ':' in t:
-      ret = 0
-      parts = t.split(':')
-      for part in parts:
-        ret = ret * 60 + int(part)
-      return ret
-    return None
-  return t or None
+    if isinstance(t, str):
+        if ":" in t:
+            ret = 0
+            parts = t.split(":")
+            for part in parts:
+                ret = ret * 60 + int(part)
+            return ret
+        return None
+    return t or None
+
 
 def time_in_minutes(t):
-  if not isinstance(t, str):
-    t = f'{t//60}:{t%60:02d}'
-  return t
+    if not isinstance(t, str):
+        t = f"{t//60}:{t%60:02d}"
+    return t
+
 
 def format_message(data):
-  scores = []
-  for s in data['scores']:
-    if s.get('name') and s.get('time'):
-      scores.append((time_in_seconds(s['time']), f'{s["name"]} {time_in_minutes(s["time"])}'))
+    scores = []
+    for s in data["scores"]:
+        if s.get("name") and s.get("time"):
+            scores.append(
+                (
+                    time_in_seconds(s["time"]),
+                    f'{s["name"]} {time_in_minutes(s["time"])}',
+                )
+            )
 
-  if not scores:
-    return None
+    if not scores:
+        return None
 
-  message = ''
-  if data['date']:
-    message = f'{data["date"]}: '
-  message += ', '.join(x[1] for x in sorted(scores))
+    message = ""
+    if data["date"]:
+        message = f'{data["date"]}: '
+    message += ", ".join(x[1] for x in sorted(scores))
 
-  return message
+    return message
+
 
 def main(argv):
-  response = requests.get(LEADERBOARD_URL, cookies=COOKIES)
-  response.raise_for_status()
-  contents = response.text
+    response = requests.get(LEADERBOARD_URL, cookies=COOKIES)
+    response.raise_for_status()
+    contents = response.text
 
-  data = parse_json(contents)
-  pprint.pprint(data)
+    data = parse_json(contents)
+    pprint.pprint(data)
 
-  if DB:
-    # DB is:
-    #   CREATE TABLE leaderboards (id INTEGER PRIMARY KEY AUTOINCREMENT, date TEXT, name TEXT, time INTEGER);
-    with contextlib.closing(sqlite3.connect(DB)) as conn:
-      with contextlib.closing(conn.cursor()) as cursor:
-        date = data['date']
-        for score in data['scores']:
-          if score['time']:
-            cursor.execute(
-              'INSERT INTO leaderboards (date, name, time) VALUES (?, ?, ?)',
-              (date, score['name'], score['time']),
-            )
-      conn.commit()
-    print('Updated', DB)
+    if DB:
+        # DB is:
+        #   CREATE TABLE leaderboards (id INTEGER PRIMARY KEY AUTOINCREMENT, date TEXT, name TEXT, time INTEGER);
+        with contextlib.closing(sqlite3.connect(DB)) as conn:
+            with contextlib.closing(conn.cursor()) as cursor:
+                date = data["date"]
+                for score in data["scores"]:
+                    if score["time"]:
+                        cursor.execute(
+                            "INSERT INTO leaderboards (date, name, time) VALUES (?, ?, ?)",
+                            (date, score["name"], score["time"]),
+                        )
+            conn.commit()
+        print("Updated", DB)
 
-  message = format_message(data)
-  if not message:
-    print('No scores, exiting')
+    message = format_message(data)
+    if not message:
+        print("No scores, exiting")
+        return
+
+    print(message)
+    if CHANNEL_ID:
+        botsay.say(CHANNEL_ID, message)
+
     return
 
-  print(message)
-  if CHANNEL_ID:
-    botsay.say(BOT_TOKEN, CHANNEL_ID, message)
-
-  return
 
 def parse_json(contents):
-  j = json.loads(contents)
-  scores = [
-    {'name': d.get('name'),
-     'rank': d.get('rank'),
-     'time': d.get('score', {}).get('secondsSpentSolving'),
-    } for d in j.get('data', [])
-  ]
-  return {
-    'date': j.get('printDate'),
-    'scores': scores,
-  }
+    j = json.loads(contents)
+    scores = [
+        {
+            "name": d.get("name"),
+            "rank": d.get("rank"),
+            "time": d.get("score", {}).get("secondsSpentSolving"),
+        }
+        for d in j.get("data", [])
+    ]
+    return {
+        "date": j.get("printDate"),
+        "scores": scores,
+    }
 
-if __name__ == '__main__':
-  main(sys.argv)
+
+if __name__ == "__main__":
+    main(sys.argv)
