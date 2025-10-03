39f19e161b011dd6d6e6c58b16bd2b686d31ebbb
Ed Bardsley <ebardsley@gmail.com>
2025-10-03 13:42:19 -0700

    env_test: add test on error message

--- env_test.py
+++ env_test.py
@@ -6,9 +6,11 @@ def test_require_env_ok():
     assert ":" in path
 
 
-def test_require_env_fail():
+def test_require_env_fail(capfd):
     try:
         env.require_env("ASDASDHLKASH")
         assert False, "expected system exit"
     except SystemExit as e:
+        captured = capfd.readouterr()
+        assert "environment variable ASDASDHLKASH is not specified" in captured.err
         assert e.code == 1
