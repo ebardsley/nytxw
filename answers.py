b432576bc5b494ed1fa1065eb4e69c68a1248be1
Ed Bardsley <ebardsley@gmail.com>
2025-11-23 10:53:49 -0800

    answers: support mini

--- answers.py
+++ answers.py
@@ -3,13 +3,14 @@ import json
 import os
 import sys
 
+import click
 import dateparser
 import requests
 
 import env
 
 
-URL = "https://www.nytimes.com/svc/crosswords/v6/puzzle/daily/{date}.json"
+URL = "https://www.nytimes.com/svc/crosswords/v6/puzzle/{game}/{date}.json"
 
 
 def puzzle_to_string(puzzle):
@@ -30,11 +31,17 @@ def puzzle_to_string(puzzle):
     return ret
 
 
-def main(argv):
+@click.command(context_settings={"show_default": True})
+@click.option("-m", "--mini", is_flag=True, default=False)
+@click.argument("date-arg", type=str)
+def main(date_arg, mini):
     cookies = {"NYT-S": env.require_env("NYTXW_COOKIE")}
     parsed_date = dateparser.parse(sys.argv[1])
     date = parsed_date.strftime("%Y-%m-%d")
-    url = URL.format(date=date)
+    url = URL.format(
+        date=date,
+        game="mini" if mini else "daily",
+    )
     print("#", url)
 
     if os.path.exists(f"{date}.json"):
@@ -50,4 +57,4 @@ def main(argv):
 
 
 if __name__ == "__main__":
-    sys.exit(main(sys.argv))
+    main()
